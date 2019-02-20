9a15da0192826f7350c0e9eaf4ff34ab81e2ba05
Joris Hens <joris@goodbytes.be>
2019-02-20 10:47:13 +0100

    Baking and unbaking POC works

--- controllers/bakery.js
+++ controllers/bakery.js
@@ -1,94 +1,120 @@
-const router = require('express').Router();
-const pngitxt = require('png-itxt');
+const router = require("express").Router();
+const pngitxt = require("png-itxt");
 const fs = require("fs");
-const isURL = require('is-url');
-const axios = require('axios');
-const bakepng = require('../libs/bakepng');
-const bakesvg = require('../libs/bakesvg');
-const modelAssertion = require('../models/assertion');
+const isURL = require("is-url");
+const axios = require("axios");
+const bakepng = require("../libs/bakepng");
+const bakesvg = require("../libs/bakesvg");
+const modelAssertion = require("../models/assertion");
 
 router.unbake = function (req, res, next) {
+  let openbadge;
 
-    let openbadge;
+  var stream = fs.createReadStream(req.file.path).pipe(
+    pngitxt.getitxt("openbadges", function (err, data) {
+      if (!err && data) {
+        console.log(data.value);
+        openbadge = data.value;
+      }
+    })
+  );
 
-    var stream = fs.createReadStream(req.file.path).pipe(
-        pngitxt.getitxt('openbadges', function (err, data) {
-            if (!err && data) {
-                console.log(data.value);
-                openbadge = data.value
-            }
-        }));
+  stream.on("end", function () {
+    if (openbadge) {
 
-    stream.on('end', function () {
-        if (openbadge) {
-            res.json({
-                status: 'success',
-                assertion: openbadge
-            });
-        } else {
-            res.status(500).send('The uploaded image is not a valid open badge.');
-        }
-    });
-
-    // console.log(req.file);
-    let isSvg = req.file.mimetype.includes('svg');
-    let isPng = req.file.mimetype.includes('png');
-}
-
-router.bake = async function (req, res, next) {
-    // get badge JSON
-    let badgeAssertion = req.body.badge;
-
-    try {
-        modelAssertion.validate(badgeAssertion);
-        console.log("🚀");
-    } catch (err) {
-        console.log(err);
-
-        let friendlyErrorMessage = err.property + " " + err.message;
+      let assertion = JSON.parse(openbadge);
+      // let's try to validate the badge 
+      try {
+        modelAssertion.validate(assertion);
+      } catch (err) {
+        let friendlyErrorMessage = err.message;
 
         res.status(500).send({
-            error: friendlyErrorMessage
-        })
-
-        return false;
-    }
-
-    // parse badge assertion and grab the badge property/url
-    let badge = req.body.badge.badge;
-
-    // see if a badge URI or document was set
-    if (!badge) {
-        throw new Error('Your badge assertion is missing the badge property (URI or BadgeClass document)');
-    }
-
-    let badgeImage;
-    // check if the badge set was a URI
-    if (isURL(badge)) {
-        // it's a URI, so we need to grab that first and look for the image there
-        await axios.get(badge).then(result => {
-            badgeImage = result.data.image;
-        }).catch(err => {
-            throw new Error(err);
+          error: friendlyErrorMessage
         });
 
-    } else {
-        badgeImage = badge.image;
-    }
+        return false;
+      }
 
-    let extension = badgeImage.substring(badgeImage.length - 3, badgeImage.length);
-    if (extension === "png") {
-        var filename = await bakepng.bake(badgeImage, badgeAssertion);
-    } else if (extension === "svg") {
-        var filename = await bakesvg.bake(badgeImage, badgeAssertion);
-    } else {
-        throw new Error("Only .png or .svg files can be baked.");
-    }
 
-    res.status(200).json({
-        status: 'success',
-        downloadUrl: filename
+      res.json({
+        status: "success",
+        assertion: openbadge
+      });
+
+    } else {
+      res.status(500).send("The uploaded image is not a valid open badge.");
+    }
+  });
+
+  // console.log(req.file);
+  let isSvg = req.file.mimetype.includes("svg");
+  let isPng = req.file.mimetype.includes("png");
+};
+
+router.bake = async function (req, res, next) {
+  // get badge JSON
+  let badgeAssertion = req.body.badge;
+
+  try {
+    modelAssertion.validate(badgeAssertion);
+  } catch (err) {
+    let friendlyErrorMessage = err.message;
+
+    res.status(500).send({
+      error: friendlyErrorMessage
     });
-}
+
+    return false;
+  }
+
+  // parse badge assertion and grab the badge property/url
+  let badge = req.body.badge.badge;
+
+  // see if a badge URI or document was set
+  if (!badge) {
+    res.status(500).send({
+      error: "Your badge assertion is missing the badge property (URI or BadgeClass document)"
+    });
+  }
+
+  let badgeImage;
+  // check if the badge set was a URI
+  if (isURL(badge)) {
+    // it's a URI, so we need to grab that first and look for the image there
+    await axios
+      .get(badge)
+      .then(result => {
+        badgeImage = result.data.image;
+      })
+      .catch(err => {
+        // e.g. 404 errors on images get caught here
+        res.status(500).send({
+          error: "The image linked in your badge could not be reached. Please make sure it exists."
+        });
+      });
+  } else {
+    badgeImage = badge.image;
+  }
+
+  let extension = badgeImage.substring(
+    badgeImage.length - 3,
+    badgeImage.length
+  );
+  if (extension === "png") {
+    var filename = await bakepng.bake(badgeImage, badgeAssertion);
+  } else if (extension === "svg") {
+    var filename = await bakesvg.bake(badgeImage, badgeAssertion);
+  } else {
+    res.status(500).send({
+      error: "Only .png or .svg files can be baked."
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    downloadUrl: filename
+  });
+};
 
 module.exports = router;
--- public/javascript/app.js
+++ public/javascript/app.js
@@ -2,29 +2,64 @@ var inputAssertion = document.querySelector("#inputAssertion");
 var bakeResult = document.querySelector(".bake__result");
 var dropZone = document.querySelector(".unbake__dropzone");
 
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+function showSweetAlert(title, type, message) {
+    Swal.fire({
+        title: title,
+        text: message,
+        type: type,
+        confirmButtonText: 'Got it',
+        confirmButtonClass: 'btn btn--primary',
+        buttonsStyling: false
+    });
+}
+
 /*
     Baking of a badge JSON into an image
 */
 document.querySelector("#btnBake").addEventListener("click", function (e) {
     // grab the Badge Assertion JSON 
-    var txtAssertion = JSON.parse(inputAssertion.value);
+    if (IsJsonString(inputAssertion.value)) {
+        var txtAssertion = JSON.parse(inputAssertion.value);
 
-    axios.post('/api/bake', {
-            badge: txtAssertion
-        })
-        .then(function (response) {
-            if (response.data.status === "success") {
-                bakeResult.innerHTML = ";;"
-                var imgBadge = document.createElement("img");
-                imgBadge.setAttribute("src", "tempbadges/" + response.data.downloadUrl);
-                imgBadge.setAttribute("alt", "Download your badge");
-                bakeResult.appendChild(imgBadge);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        axios.post('/api/bake', {
+                badge: txtAssertion
+            })
+            .then(function (response) {
+                if (response.data.status === "success") {
+                    bakeResult.innerHTML = "";
+                    var imgBadge = document.createElement("img");
+                    imgBadge.setAttribute("src", "tempbadges/" + response.data.downloadUrl);
+                    imgBadge.setAttribute("alt", "Download your badge");
+                    imgBadge.setAttribute("class", "badge--baked");
+                    bakeResult.appendChild(imgBadge);
 
+                    Swal.fire({
+                        title: "Right-click to download",
+                        html: imgBadge,
+                        confirmButtonText: 'Got it!',
+                        confirmButtonClass: 'btn btn--primary',
+                        buttonsStyling: false
+                    });
+
+
+                }
+            })
+            .catch(function (error) {
+                var errorMessage = error.response.data.error;
+                showSweetAlert("Oops", "error", errorMessage);
+            });
+    } else {
+        showSweetAlert("Invalid badge", "error", "The badge JSON you entered is not a valid JSON string.");
+    }
     e.preventDefault();
 });
 
@@ -42,9 +77,23 @@ dropZone.addEventListener("drop", function (e) {
 
     axios.post('/api/unbake', formData)
         .then(function (res) {
-            console.log(res);
-        }).catch(function (e) {
-            console.log(e);
+            if (res.data.status === "success") {
+                Swal.fire({
+                    type: "success",
+                    html: `
+                        <div class="unbake__result">
+                        <h3 class="unbake__result__title">This badge looks legit!</h3>
+                        <pre class="unbake__result__assertion">${res.data.assertion}</pre>
+                        </div>
+                    `,
+                    confirmButtonText: 'Got it!',
+                    confirmButtonClass: 'btn btn--primary',
+                    buttonsStyling: false
+                });
+            }
+        }).catch(function (error) {
+            var errorMessage = error.response.data.error;
+            showSweetAlert("Oops", "error", errorMessage);
         })
 
     e.preventDefault();
